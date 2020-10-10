db243296faac1d9dce16fc692912c04930f28b3b
钱赛龙 <m13186571180@163.com>
2020-10-10 14:09:19 +0800

    提交;

--- 01-arithmetic.js
+++ 01-arithmetic.js
@@ -111,3 +111,129 @@
     return result;
   }
 })();
+
+// 6-防抖
+(function () {
+  function debounce (fn, wait, immediate) {
+    let timer = null;
+    return function (...args) {
+      // 立即执行的功能(timer为空表示首次触发)
+      if (immediate && !timer) {
+        fn.apply(this, args);
+      }
+      // 有新的触发，则把定时器清空
+      timer && clearTimeout(timer);
+      // 重新计时
+      timer = setTimeout(() => {
+        fn.apply(this, args);
+      }, wait)
+    }
+  }
+})();
+
+// 7-节流
+(function () {
+  // 时间戳版本
+  function throttle (fn, wait) {
+    // 上一次执行时间
+    let previous = 0;
+    return function (...args) {
+      // 当前时间
+      let now = +new Date();
+      if (now - previous > wait) {
+        previous = now;
+        fn.apply(this, args);
+      }
+    }
+  }
+
+  // 定时器版本
+  function throttle (fn, wait) {
+    let timer = null;
+    return function (...args) {
+      if (!timer) {
+        timer = setTimeout(() => {
+          fn.apply(this, args);
+          timer = null;
+        }, wait)
+      }
+    }
+  }
+})();
+
+// 8-深拷贝
+(function () {
+  // 乞巧版
+  function cloneDeep1 (source) {
+    return JSON.parse(JSON.stringify(source));
+  }
+
+  // 递归版
+  function cloneDeep2 (source) {
+    // 如果输入的为基本类型，直接返回
+    if (!(typeof source === 'object' && source !== null)) {
+      return source;
+    }
+
+    // 判断输入的为数组函数对象，进行相应的构建
+    const target = Array.isArray(source) ? [] : {};
+
+    for (let key in source) {
+      // 判断是否是自身属性
+      if (Object.prototype.hasOwnProperty.call(source, key)) {
+        if (typeof source === 'object' && source !== null) {
+          target[key] = cloneDeep2(source[key]);
+        } else {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    return target;
+  }
+
+  // 循环方式
+  function cloneDeep3 (source) {
+    if (!(typeof source === 'object' && source !== null)) {
+      return source;
+    }
+
+    const root = Array.isArray(source) ? [] : {};
+    // 定义一个栈
+    const loopList = [{
+      parent: root,
+      key: undefined,
+      data: source,
+    }];
+
+    while (loopList.length > 0) {
+      // 深度优先
+      const node = loopList.pop();
+      const parent = node.parent;
+      const key = node.key;
+      const data = node.data;
+
+      // 初始化赋值目标，key为undefined则拷贝到父元素，否则拷贝到子元素
+      let res = parent;
+      if (typeof key !== 'undefined') {
+        res = parent[key] = Array.isArray(data) ? [] : {};
+      }
+
+      for (let key in data) {
+        if (data.hasOwnProperty(key)) {
+          if (typeof data[key] === 'object' && data !== null) {
+            loopList.push({
+              parent: res,
+              key: key,
+              data: data[key],
+            });
+          } else {
+            res[key] = data[key];
+          }
+        }
+      }
+    }
+
+    return root;
+  }
+})();
